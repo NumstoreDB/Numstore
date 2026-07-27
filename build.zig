1423519d0d35f53841cc145cdcdf1fb962c2316e
LinckeTheo <lincketheo@gmail.com>
2026-07-26 21:50:05 -0400

    cleans up the build script - adds c examples back

--- build.zig
+++ build.zig
@@ -2,9 +2,19 @@ const std = @import("std");
 const zcc = @import("zig_compile_commands");
 
 pub fn build(b: *std.Build) !void {
+    const build_samples = b.option(
+        bool,
+        "samples",
+        "Build the sample executables",
+    ) orelse false;
+
+    // A list of targets for zcc
     var targets: std.ArrayList(*std.Build.Step.Compile) = .empty;
+
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
+    // Gather all c source files for the library
     const csrcs = try glob(b.allocator, b.graph.io, "src", ".c");
 
     // Build Numstore
@@ -28,14 +38,63 @@ pub fn build(b: *std.Build) !void {
         },
     });
     numstore.root_module.addIncludePath(b.path("src"));
-    try targets.append(b.allocator, numstore);
     b.installArtifact(numstore);
+    try targets.append(b.allocator, numstore);
 
-    // Build Zig Samples
-    try build_zig_sample(b, &targets, "ns_sample1", b.path("src/samples/ns_sample1_basic.zig"), target, optimize, numstore);
-    try build_zig_sample(b, &targets, "ns_sample2", b.path("src/samples/ns_sample2_struct.zig"), target, optimize, numstore);
-    try build_zig_sample(b, &targets, "ns_sample3", b.path("src/samples/ns_sample3_transactions.zig"), target, optimize, numstore);
-    try build_c_sample(b, &targets, "ns_sample4", b.path("src/samples/main.c"), target, optimize, numstore);
+    if(build_samples) {
+        // Build Zig Samples
+        try build_zig_sample(
+            b, &targets, 
+            "ns_zig_sample1", 
+            b.path("src/samples/ns_sample1_basic.zig"), 
+            target, optimize, numstore);
+
+        try build_zig_sample(
+            b, &targets, 
+            "ns_zig_sample2", 
+            b.path("src/samples/ns_sample2_struct.zig"), 
+            target, optimize, numstore);
+
+        try build_zig_sample(
+            b, &targets, 
+            "ns_zig_sample3", 
+            b.path("src/samples/ns_sample3_transactions.zig"), 
+            target, optimize, numstore);
+
+        // Build c Samples
+        try build_c_sample(
+            b, &targets, 
+            "ns_c_sample1", 
+            b.path("src/samples/ns_sample1_basic_crud.c"), 
+            target, optimize, numstore);
+
+        try build_c_sample(
+            b, &targets, 
+            "smfile_c_sample1", 
+            b.path("src/samples/smfile_sample1_basic_crud.c"), 
+            target, optimize, numstore);
+
+        try build_c_sample(
+            b, &targets, 
+            "smfile_c_sample2", 
+            b.path("src/samples/smfile_sample2_transactions.c"), 
+            target, optimize, numstore);
+
+        try build_c_sample(
+            b, &targets, 
+            "smfile_c_sample3", 
+            b.path("src/samples/smfile_sample3_stride.c"), 
+            target, optimize, numstore);
+
+        try build_c_sample(
+            b, &targets, 
+            "smfile_c_sample4", 
+            b.path("src/samples/smfile_sample4_rollback_commit.c"), 
+            target, optimize, numstore);
+    }
+
+    // Add compile_commands.json for c family language servers
+    _ = zcc.createStep(b, "cdb", try targets.toOwnedSlice(b.allocator));
 }
 
 pub fn build_zig_sample(
@@ -74,6 +133,7 @@ pub fn build_c_sample(
     optimize: ?std.builtin.OptimizeMode,
     numstore: *std.Build.Step.Compile, 
 ) !void {
+    // 
     const sample = b.addExecutable(.{
         .name = name,
         .linkage = .dynamic,
@@ -91,8 +151,6 @@ pub fn build_c_sample(
     sample.root_module.addIncludePath(b.path("src"));
     b.installArtifact(sample);
     try targets.append(b.allocator, sample);
-
-    _ = zcc.createStep(b, "cdb", targets.toOwnedSlice(b.allocator) catch @panic("OOM"));
 
 }
 
--- src/samples/main.c
+++
@@ -1,8 +0,0 @@
-#include "numstore.h"
-
-int
-main ()
-{
-  nsdb_t *db = nsdb_open ("foo");
-  nsdb_close (db);
-}
